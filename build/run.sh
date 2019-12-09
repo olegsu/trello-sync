59649b09488821e6a17d3d15f4e45baad33a43ca
Oleg Sucharevich <olegs@codefresh.io>
2019-12-09 19:31:31 +0200

    add run script

---
+++ build/run.sh
@@ -0,0 +1,6 @@
+echo "Cleaning old logs directory"
+rm -rf /Users/olsynt/workspace/personal/trello-sync/logs/* || true
+echo "Building binary"
+go build -o trello-sync .
+echo "Running..."
+./trello-sync sync --logs ./logs --trello-app-key $TRELLO_APP_KEY --trello-token $TRELLO_TOKEN --trello-board-id $TRELLO_BOARD_ID --store ./logs/store.yaml --google-service-account personal-710e359b2eee.json --google-spreadsheet-id $GOOGLE_SPREADSHEET_ID --trello-service $TRELLO_SERVICE_PATH --google-spreadsheet-service $GOOGLE_SERVICE_PATH
