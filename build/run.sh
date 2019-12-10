89c34a8fa0a2688de6991611882483ac77631db2
Oleg Sucharevich <olegs@codefresh.io>
2019-12-10 11:53:37 +0200

    update run script

--- build/run.sh
+++ build/run.sh
@@ -3,4 +3,4 @@ rm -rf /Users/olsynt/workspace/personal/trello-sync/logs/* || true
 echo "Building binary"
 go build -o trello-sync .
 echo "Running..."
-./trello-sync sync --logs ./logs --trello-app-key $TRELLO_APP_KEY --trello-token $TRELLO_TOKEN --trello-board-id $TRELLO_BOARD_ID --store ./logs/store.yaml --google-service-account $GOOGLE_SERVICE_ACCOUNT_PATH --google-spreadsheet-id $GOOGLE_SPREADSHEET_ID --trello-service $TRELLO_SERVICE_PATH --google-spreadsheet-service $GOOGLE_SERVICE_PATH
+./trello-sync sync --trello-app-key $TRELLO_APP_KEY --trello-token $TRELLO_TOKEN --trello-board-id $TRELLO_BOARD_ID --google-service-account $GOOGLE_SERVICE_ACCOUNT_PATH --google-spreadsheet-id $GOOGLE_SPREADSHEET_ID --trello-service $TRELLO_SERVICE_PATH --google-spreadsheet-service $GOOGLE_SERVICE_PATH
